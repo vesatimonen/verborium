2126002defb29959b4f0ae1398282c1f7fe82d0a
Vesa Timonen <vtimonen@gmail.com>
2025-02-27 11:09:15 +0200

    Remove grid element

--- js/globals.js
+++ js/globals.js
@@ -22,7 +22,6 @@ var options = {
 const elements = {
     screen:         document.getElementById("game-screen"),
     board:          document.getElementById("game-board"),
-    grid:           document.getElementById("game-grid"),
     canvas:         document.getElementById('game-canvas'),
     gameOver:       document.getElementById("game-over-modal"),
 
