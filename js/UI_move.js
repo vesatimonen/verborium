d4b3d1e204451f57fcd1b2e5fa91627cfac17872
Vesa Timonen <vtimonen@gmail.com>
2025-03-03 07:48:11 +0200

    Save point

--- js/UI_move.js
+++ js/UI_move.js
@@ -31,45 +31,31 @@ function uiMovePosition(event) {
 function uiClick(event) {
     /* Get clock position */
     move = uiMovePosition(event);
-    X = move.X;
-    Y = move.Y;
-
-    /* Closest wall */
-    Xwall = Math.round(X);
-    Ywall = Math.round(Y);
-
-    /* Current cell */
-    Xcell = Math.floor(X);
-    Ycell = Math.floor(Y);
+    X = Math.floor(move.X);
+    Y = Math.floor(move.Y);
 
     /* Check limits */
-    if (Ycell < 0 || Ycell >= globals.game.board.height ||
-        Xcell < 0 || Xcell >= globals.game.board.width) {
+    if (Y < 0 || Y >= globals.game.board.height ||
+        X < 0 || X >= globals.game.board.width) {
+
         return false;
     }
 
-    /* Direction from wall */
-    Xdelta = Math.abs(X - Xwall);
-    Ydelta = Math.abs(Y - Ywall);
-    if (Xdelta < Ydelta) {
-        if (Ydelta - Xdelta > 0.0) {
-            /* Vertical wall */
-            if (Xwall > 0 && Xwall < globals.game.board.width) {
-//                globals.game.makeMove("vertical", Xwall, Ycell);
-                uiRedraw();
-            }
-        }
-    } else {
-        if (Xdelta - Ydelta > 0.0) {
-            /* Horizontal wall */
-            if (Ywall > 0 && Ywall < globals.game.board.height) {
-//                globals.game.makeMove("horizontal", Xcell, Ywall);
-                uiRedraw();
-            }
-        }
-    }
+console.log(X, Y);
+
+    /* Make move */
+//    globals.game.makeMove("vertical", Xwall, Ycell);
+
+    /* Redraw UI */
+//    uiRedraw();
 }
 
+
+
+
+/*****************************************************************************
+ * Register game board event handlers
+ *****************************************************************************/
 elements.board.addEventListener("click", uiClick, {passive: true});
 
 if (false) {
