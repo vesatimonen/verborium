da4c9613a5ec963f4b78905f443aedede6564651
Vesa Timonen <vtimonen@gmail.com>
2025-03-05 08:57:58 +0200

    Save point

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
 /*****************************************************************************
  * Game challenges (levels)
  *****************************************************************************/
-const challengeSetDefault = [
-    {info: "INFO: W05 R00000-01000 S0-1-2-2-0-0 T000039 >4x4-FIN-tus-koi-tar-oi-suus-li-dol-ke-ko-pak-mah-a-nen-lai-ma-suo"},
+const challengeSetFIN = [
     {info: "INFO: W05 R00000-01000 S0-1-2-2-0-0 T000039 >4x4-FIN-tus-koi-tar-oi-suus-li-dol-ke-ko-pak-mah-a-nen-lai-ma-suo"},
     {info: "INFO: W10 R00000-01000 S0-2-3-3-1-1 T000198 >6x6-FIN-nen-li-e-del-li-no-vä-del-lou-ta-nen-huo-sel-pa-ke-a-kit-mer-pi-rem-oi-vä-tä-nen-me-rik-nen-nen-to-köi-a-ka-lai-li-del-nä"},
     {info: "INFO: W26 R01000-04000 S0-18-5-2-1-0 T000294 >8x8-FIN-taa-da-oi-kart-taa-lä-nel-ken-lait-soi-keut-taa-kai-taa-haas-työs-na-ly-tää-kat-vaa-koo-teet-tää-a-työl-lis-taa-tää-ta-taa-polt-liit-tää-peit-syö-syyt-la-puol-juos-tää-ke-hu-dä-ha-tel-taa-ta-da-hy-a-ta-lu-mis-laa-lau-soi-mäh-tää-tää-kier-val-da-juo"},
 ];
 
+const challengeSetSWE = [
+    {info: "INFO: W05(7.4) R00000-03000 P3.2 L0-2-1-1-1-0 T000070 >4x4-SWE-fr-ämst-ap-k-kong-ress-nt-tens-akt-u-a-e-ll-e-eleg-v"},
+];
+
+const challengeSetDefault = challengeSetFIN;
 
 /*****************************************************************************
  * Parse URL options
